e78f3129d1035f2427a95d5a745dad925baf9d82
Jaime Menendez <jaimemenendezalvarez@gmail.com>
2021-08-05 10:09:13 -0500

    Color Theme preview when hover the Logo Divs

--- HTML/js/index.js
+++ HTML/js/index.js
@@ -16,33 +16,92 @@ togglePassword.addEventListener('click', function (e) {
   const bloqueAzul = document.querySelector(".logo .bloqueAzul");
   const bloqueVerde = document.querySelector(".logo .bloqueVerde");
   const bloqueNaranja = document.querySelector(".logo .bloqueNaranja");
-  
+  let currentColor = '#EA4533';
+  let currentColorHover = '#c0392b';
+
   const root = document.documentElement;
 
-  bloqueRojo.addEventListener('click',e =>{
+bloqueRojo.addEventListener('click',e =>{
     root.style.setProperty('--buttonColor', '#EA4533');
     root.style.setProperty('--buttonColorHover', '#c0392b');
-    console.log(e);
+    currentColor = root.style.getPropertyValue('--buttonColor');
+    currentColorHover = root.style.getPropertyValue('--buttonColorHover');
 })
+
+bloqueRojo.addEventListener('mouseover', e =>{
+  currentColor = root.style.getPropertyValue('--buttonColor');
+  currentColorHover = root.style.getPropertyValue('--buttonColorHover');
+  root.style.setProperty('--buttonColor', '#EA4533');
+  root.style.setProperty('--buttonColorHover', '#c0392b');
+})
+
+bloqueRojo.addEventListener('mouseout', e =>{
+  root.style.setProperty('--buttonColor', currentColor);
+  root.style.setProperty('--buttonColorHover', currentColorHover);
+})
+
+
 
 bloqueAzul.addEventListener('click',e =>{
     root.style.setProperty('--buttonColor', '#585DBA');
     root.style.setProperty('--buttonColorHover', '#3f438b');
-    console.log(e);
+    currentColor = root.style.getPropertyValue('--buttonColor');
+    currentColorHover = root.style.getPropertyValue('--buttonColorHover');
 })
+
+bloqueAzul.addEventListener('mouseover', e =>{
+  currentColor = root.style.getPropertyValue('--buttonColor');
+  currentColorHover = root.style.getPropertyValue('--buttonColorHover');
+  root.style.setProperty('--buttonColor', '#585DBA');
+  root.style.setProperty('--buttonColorHover', '#3f438b');
+})
+
+bloqueAzul.addEventListener('mouseout', e =>{
+  root.style.setProperty('--buttonColor', currentColor);
+  root.style.setProperty('--buttonColorHover', currentColorHover);
+})
+
+
 
 bloqueVerde.addEventListener('click',e =>{
     root.style.setProperty('--buttonColor', '#44979c');
     root.style.setProperty('--buttonColorHover', '#306b6e');
-    console.log(e);
+    currentColor = root.style.getPropertyValue('--buttonColor');
+    currentColorHover = root.style.getPropertyValue('--buttonColorHover');
 })
+bloqueVerde.addEventListener('mouseover', e =>{
+  currentColor = root.style.getPropertyValue('--buttonColor');
+  currentColorHover = root.style.getPropertyValue('--buttonColorHover');
+  root.style.setProperty('--buttonColor', '#44979c');
+  root.style.setProperty('--buttonColorHover', '#306b6e');
+})
+
+bloqueVerde.addEventListener('mouseout', e =>{
+  root.style.setProperty('--buttonColor', currentColor);
+  root.style.setProperty('--buttonColorHover', currentColorHover);
+})
+
 
 
 bloqueNaranja.addEventListener('click',e =>{
     root.style.setProperty('--buttonColor', '#F4AE62');
     root.style.setProperty('--buttonColorHover', '#c78c4d');
-    console.log(e);
+    currentColor = root.style.getPropertyValue('--buttonColor');
+    currentColorHover = root.style.getPropertyValue('--buttonColorHover');
 })
+bloqueNaranja.addEventListener('mouseover', e =>{
+  currentColor = root.style.getPropertyValue('--buttonColor');
+  currentColorHover = root.style.getPropertyValue('--buttonColorHover');
+  root.style.setProperty('--buttonColor', '#F4AE62');
+  root.style.setProperty('--buttonColorHover', '#c78c4d');
+})
+
+bloqueNaranja.addEventListener('mouseout', e =>{
+  root.style.setProperty('--buttonColor', currentColor);
+  root.style.setProperty('--buttonColorHover', currentColorHover);
+})
+
+
 
 if(togglePasswordConfirm){
 togglePasswordConfirm.addEventListener('click', function (e) {
