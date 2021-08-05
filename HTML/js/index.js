39584e666da5ed6bd94df53bbabeebf9063f475d
Jaime Menendez <jaimemenendezalvarez@gmail.com>
2021-08-05 01:25:53 -0500

    Change color script was included in index.js. Fixed Problems

--- HTML/js/index.js
+++ HTML/js/index.js
@@ -12,10 +12,44 @@ togglePassword.addEventListener('click', function (e) {
   this.classList.toggle('flaticon-visibility');
 });
 
+
+
+  const bloqueRojo = document.querySelector(".logo .bloqueRojo");
+  const bloqueAzul = document.querySelector(".logo .bloqueAzul");
+  const bloqueVerde = document.querySelector(".logo .bloqueVerde");
+  const bloqueNaranja = document.querySelector(".logo .bloqueNaranja");
+  
+  const root = document.documentElement;
+
+  bloqueRojo.addEventListener('click',e =>{
+    root.style.setProperty('--buttonColor', '#EA4533');
+    root.style.setProperty('--buttonColorHover', '#c0392b');
+    console.log(e);
+})
+
+bloqueAzul.addEventListener('click',e =>{
+    root.style.setProperty('--buttonColor', '#585DBA');
+    root.style.setProperty('--buttonColorHover', '#3f438b');
+    console.log(e);
+})
+
+bloqueVerde.addEventListener('click',e =>{
+    root.style.setProperty('--buttonColor', '#44979c');
+    root.style.setProperty('--buttonColorHover', '#306b6e');
+    console.log(e);
+})
+
+
+bloqueNaranja.addEventListener('click',e =>{
+    root.style.setProperty('--buttonColor', '#F4AE62');
+    root.style.setProperty('--buttonColorHover', '#c78c4d');
+    console.log(e);
+})
+
 togglePasswordConfirm.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = passwordConfirm.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordConfirm.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('flaticon-visibility');
-  });
+  // toggle the type attribute
+  const type = passwordConfirm.getAttribute('type') === 'password' ? 'text' : 'password';
+  passwordConfirm.setAttribute('type', type);
+  // toggle the eye slash icon
+  this.classList.toggle('flaticon-visibility');
+});
