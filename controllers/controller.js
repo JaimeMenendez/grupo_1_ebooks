7e7f01f8ca5256b252d34a71655f7eab7a1e784b
TatianaLeal1125 <mileydy.1125@gmail.com>
2021-08-28 15:54:35 -0500

    Created secciones.json, categorias.json and botonAInicio.ejs and fixed problem with books on home

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,39 +1,39 @@
-const path = require('path');
-const book = require('./libro.json');
-const carrito = require('./carrito.json');
-const section = require('./section.json');
-
+const path = require('path')
+const book = require('./libro.json')
+const carrito = require('./carrito.json')
+const seccion = require('./secciones.json')
+const categorias = require('./categorias.json')
 
 const controller = {
-    sendHome: (req, res) => {
-        res.render("home", section);
-    },
-    sendPoliticaDePrivacidad: (req, res) => {
-        res.render("documento", { documento: "./partials/politica" });
-    },
-    sendTerminosyCondiciones: (req, res) => {
-        res.render("documento", { documento: "./partials/terminos" });
-    },
-    sendShoppingCart: (req, res) => {
-        res.render("carrito", carrito);
-    },
-    sendProductDescription: (req, res) => {
-        res.render("description", book);
-    },
-    sendLogin: (req, res) => {
-        const htmlPath = path.resolve(__dirname, "../views/login.html");
-        res.sendFile(htmlPath);
-    },
-    sendRegister: (req, res) => {
-        const htmlPath = path.resolve(__dirname, "../views/register.html");
-        res.sendFile(htmlPath);
-    },
-    sendSearchPage: (req, res) => {
-        res.render("searchBook", section);
-    },
-    sendPageNotFound: (req, res) => {
-        res.render("error404");
-    }
+  sendHome: (req, res) => {
+    res.render('home', { seccion: seccion, categorias: categorias })
+  },
+  sendPoliticaDePrivacidad: (req, res) => {
+    res.render('documento', { documento: './partials/politica' })
+  },
+  sendTerminosyCondiciones: (req, res) => {
+    res.render('documento', { documento: './partials/terminos' })
+  },
+  sendShoppingCart: (req, res) => {
+    res.render('carrito', carrito)
+  },
+  sendProductDescription: (req, res) => {
+    res.render('description', book)
+  },
+  sendLogin: (req, res) => {
+    const htmlPath = path.resolve(__dirname, '../views/login.html')
+    res.sendFile(htmlPath)
+  },
+  sendRegister: (req, res) => {
+    const htmlPath = path.resolve(__dirname, '../views/register.html')
+    res.sendFile(htmlPath)
+  },
+  sendSearchPage: (req, res) => {
+    res.render('searchBook', seccion)
+  },
+  sendPageNotFound: (req, res) => {
+    res.render('error404')
+  }
 }
 
-module.exports = controller;
+module.exports = controller
--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,17 @@
-const path = require('path');
-const express = require('express');
-const controller = require('../controllers/controller');
-const router = express.Router();
+const express = require('express')
+const controller = require('../controllers/controller')
+const router = express.Router()
 
+router.get('/', controller.sendHome)
+router.get('/home', controller.sendHome)
+router.get('/PoliticaDePrivacidad', controller.sendPoliticaDePrivacidad)
+router.get('/TerminosyCondiciones', controller.sendTerminosyCondiciones)
+router.get('/carrito', controller.sendShoppingCart)
+router.get('/description', controller.sendProductDescription)
+router.get('/login', controller.sendLogin)
+router.get('/register', controller.sendRegister)
+router.get('/searchBook', controller.sendSearchPage)
 
+router.get('*', controller.sendPageNotFound)
 
-router.get("/", controller.sendHome);
-router.get("/home", controller.sendHome);
-router.get("/PoliticaDePrivacidad", controller.sendPoliticaDePrivacidad)
-router.get("/TerminosyCondiciones", controller.sendTerminosyCondiciones)
-router.get("/carrito", controller.sendShoppingCart)
-router.get("/description", controller.sendProductDescription)
-router.get("/login",controller.sendLogin)
-router.get("/register", controller.sendRegister)
-router.get("/searchBook", controller.sendSearchPage)
-
-router.get("*", controller.sendPageNotFound)
-
-module.exports = router;
+module.exports = router
