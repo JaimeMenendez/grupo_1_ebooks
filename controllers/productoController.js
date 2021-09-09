f83a404c5b62aaa5858dca6bfebf3327339802db
Jaime Menendez <jaimemenendezalvarez@gmail.com>
2021-09-09 16:44:11 -0500

    Added Category and Subcategory to AddBook

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -24,30 +24,48 @@ const productoController = {
     res.render('error404')
   },
   agregarLibroView: (req, res) => {
-    res.render('products/agregarLibro', { tittle: '<i class="fas fa-book"></i>&nbsp Agregar Libro' })
+    res.render('products/editar-agregar-producto', {
+      tittle: '<i class="fas fa-book"></i>&nbsp Agregar Libro'
+    })
   },
 
   agregarLibro: (req, res) => {
-    let librosDB = readFileSync('./DB/librosDB.json', 'utf-8')
-    if (librosDB == '') {
-      librosDB = []
-    } else {
-      librosDB = JSON.parse(librosDB)
-    }
-
     const newBook = req.body
-    newBook.id = librosDB[0].id_count + 1
-    librosDB[0].id_count = librosDB[0].id_count + 1
+    newBook.precioBook = Number.parseInt(newBook.precioBook)
+    newBook.precioEbook = Number.parseInt(newBook.precioEbook)
+
+    let librosDB = readFileSync('./DB/librosDB.json', 'utf-8')
+    librosDB = JSON.parse(librosDB)
+
+    const currentMaxId = librosDB[librosDB.length - 1].id
+    newBook.id = currentMaxId + 1
+    newBook.votos = 0
+    newBook.rating = 0
+
     if (req.file) {
       newBook.portada = req.file.path
+    } else {
+      newBook.portada = 'public/images/booksCover/default-image.png'
     }
 
     librosDB.push(newBook)
-    writeFileSync('./DB/librosDB.json', JSON.stringify(librosDB, null, 2), 'utf-8')
-    res.render('products/agregarLibro', { tittle: '<i class="fas fa-plus-circle"></i>&nbsp Agregar Libro' })
+    writeFileSync('./DB/librosDB.json', JSON.stringify(librosDB, null, 2))
+    res.redirect('/producto/' + newBook.id)
   },
   editarLibroView: (req, res) => {
-    res.render('agregarLibro', { tittle: '<i class="fas fa-edit"></i>&nbsp Editar Libro' })
+    res.render('products/editar-agregar-producto', {
+      tittle: '<i class="fas fa-edit"></i>&nbsp Editar Libro'
+    })
+  }
+}
+
+function eliminarImagen(req, index) {
+  if (products[index].image !== 'default-image.png') {
+    try {
+      fs.unlinkSync(path.resolve(productsImageFolder, products[index].image))
+    } catch {
+      console.log('El archivo no existe')
+    }
   }
 }
 
