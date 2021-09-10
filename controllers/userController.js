5d29c8a2322e0b9a86d54ba04fcb8eecc16eaa9d
Jaime Menendez <jaimemenendezalvarez@gmail.com>
2021-09-10 12:41:45 -0500

    Address section added in MyAccount View

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
 const seccion = require('./secciones.json')
 const botonesPrincipales = require('./botonesPrincipales.json')
+const user = require('./user.json')
 
 const userController = {
   sendMyAccount: (req, res) => {
-    res.render('users/myAccount', { botonesPrincipales: botonesPrincipales, busquedas: seccion.busquedas, favoritos: seccion.favoritos })
+    res.render('users/myAccount', { user, botonesPrincipales: botonesPrincipales, busquedas: seccion.busquedas, favoritos: seccion.favoritos })
   },
   sendSecurity: (req, res) => {
     res.render('users/security', { busquedas: seccion.busquedas, favoritos: seccion.favoritos })
