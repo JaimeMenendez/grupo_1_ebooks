84c785509a12ca1d68b115031e25125818cf9f41
Jaime Menendez <jaimemenendezalvarez@gmail.com>
2021-11-08 13:20:23 -0600

    Added database with docker-compose

--- database/config/config.js
+++ database/config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "1234",
+    "password": "",
     "database": "insidebooksDB",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": "1234",
+    "password": "",
     "database": "insidebooksDB",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": "1234",
+    "password": "",
     "database": "insidebooksDB",
     "host": "127.0.0.1",
     "dialect": "mysql"
